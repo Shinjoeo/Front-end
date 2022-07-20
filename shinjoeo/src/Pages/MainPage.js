c8dc208f01f1d514e9e8d48f589dec62200f3fa3
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-07-21 02:00:38 +0900

    modify mainpage

--- shinjoeo/src/Pages/MainPage.js
+++ shinjoeo/src/Pages/MainPage.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../Components/Header';
+import List from '../Components/List';
 import '../style/MainPage.css';
 
 const MainPage = () => {
     return (
-        <section className='main-container'>
-            <Header/>
-        </section>
+        <div className='main-container'>
+            <div className='inner-container'>
+                {/* 헤더 제외 컴포넌트 넣으면 됨 */}
+                <List/>
+            </div>
+        </div>
     );
 };
 
