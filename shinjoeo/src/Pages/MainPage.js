0edaf84ddef75c40e6e0042307e66d8cc4ad76c3
forever2969 <forever296@naver.com>
2022-07-21 03:11:47 +0900

    filter css modify and merge

--- shinjoeo/src/Pages/MainPage.js
+++ shinjoeo/src/Pages/MainPage.js
@@ -3,8 +3,22 @@ import Header from '../Components/Header';
 import SearchBar from '../Components/SearchBar';
 import List from '../Components/List';
 import '../style/MainPage.css';
+import {useState} from 'react';
 
 const MainPage = () => {
+
+    const [isActiveNew, setIsActiveNew] = useState(true);
+    const [isActivePopular, setIsActivePopular] = useState(false);
+
+    const handleClickNew = ()=>{
+        setIsActiveNew(!isActiveNew);
+        setIsActivePopular(!isActivePopular);
+    };
+    const handleClickPopular = ()=>{
+        setIsActivePopular(!isActivePopular);
+        setIsActiveNew(!isActiveNew);
+    };
+
     return (
         <div className='main-container'>
             <Header />
@@ -12,8 +26,8 @@ const MainPage = () => {
             <div className='inner-container'>
                 <SearchBar />
                 <div id="filterDiv">
-                    <button type="button" className='filterBtn'>최신순</button>
-                    <button type="button" className='filterBtn'>인기순</button>
+                    <button type="button" className={isActiveNew ? 'dontClickBtn' : 'clickBtn'} onClick={handleClickNew}>최신순</button>
+                    <button type="button" className={isActivePopular ? 'dontClickBtn' : 'clickBtn'} onClick={handleClickPopular}>인기순</button>
                 </div>
                 {/* 헤더 제외 컴포넌트 넣으면 됨 */}
                 <List/>
