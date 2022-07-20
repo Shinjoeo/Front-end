fe848fd3667569ad697f0ae908bbda30fa832e45
JaeHyunGround <gothddlek@naver.com>
2022-07-21 00:30:50 +0900

    업로드 페이지 통신 제외 구현

---
+++ shinjoeo/src/Components/WriteForm.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom'
+import "../style/WriteForm.css"
+
+const WriteForm = () => {
+
+    const [word, setWord] = useState("");
+    const [explain, setExplain] = useState("");
+
+    const navigate = useNavigate();
+
+    const wordControl = (e) => {
+        setWord(e.target.value)
+    }
+    const explainControl = (e) => {
+        setExplain(e.target.value)
+    }
+    const goHome = () => {
+        navigate('/');
+        // setWord("");
+        // setExplain("");
+    }
+
+    return (
+        <>
+            <div className='word'>
+                <input className='wordinput' onChange={wordControl} value={word} placeholder="등록 할 신조어를 입력 해 주세요." />
+            </div>
+            <div className='explain'>
+                <textarea className='explaininput' onChange={explainControl} value={explain} placeholder="의미를 입력 해 주세요." />
+            </div>
+            <div className='upload'>
+                <button className='uploadbtn' onClick={goHome}>신조어 등록하기</button>
+            </div>
+        </>
+    );
+}
+
+export default WriteForm;
--- shinjoeo/src/Pages/UploadPage.js
+++ shinjoeo/src/Pages/UploadPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import WriteForm from '../Components/WriteForm';
+import Header from '../Components/Header';
+import '../style/UploadPage.css'
 
 const UploadPage = () => {
     return (
-        <div>
-            
-        </div>
+        <section className='upload-container'>
+            {/* <Header /> */}
+            <WriteForm />
+        </section>
     );
 };
 
