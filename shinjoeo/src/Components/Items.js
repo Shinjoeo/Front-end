3ad68eab9ed07ad6ce696c1881ef441c80a6bb22
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-07-21 01:43:42 +0900

    add Items component

---
+++ shinjoeo/src/Components/Items.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../style/Items.css';
+
+const Items = () => {
+
+    return (
+        <section className='item-container'>
+            <div>1</div>
+            <div>
+                <div>name</div>
+                <div>explain</div>
+            </div>
+            <div>
+                <div>하트</div>
+                <div>토글 버튼</div>
+            </div>
+        </section>
+    );
+};
+
+export default Items;
