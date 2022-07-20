9cf3bb9caae46765d0d422c6d4c8bb0cb823bddd
forever2969 <forever296@naver.com>
2022-07-21 02:10:40 +0900

    merge Component

--- shinjoeo/src/Pages/MainPage.js
+++ shinjoeo/src/Pages/MainPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from '../Components/Header';
+import SearchBar from '../Components/SearchBar';
 import List from '../Components/List';
 import '../style/MainPage.css';
 
 const MainPage = () => {
     return (
         <div className='main-container'>
+            <Header />
             <div className='inner-container'>
+                <SearchBar />
                 {/* 헤더 제외 컴포넌트 넣으면 됨 */}
                 <List/>
             </div>
