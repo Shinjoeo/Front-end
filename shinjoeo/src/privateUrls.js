820fdffcb0365956d0d012ac57c86bbe553e4dec
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-07-23 06:31:06 +0900

    add private info

--- shinjoeo/src/privateUrls.js
+++ shinjoeo/src/privateUrls.js
@@ -1,7 +1,7 @@
-const PROXY_BASE_URL = "http://ec2-54-180-8-2.ap-northeast-2.compute.amazonaws.com:8000";
+const PROXY_BASE_URL = "http://shinjoeo.s3-website.ap-northeast-2.amazonaws.com";
 
 const REST_API_KEY = 'fad3300d7c33374e2bb2bab358bcbec3';
-const REDIRECT_URI = 'http://localhost:3000/callback'; //백이랑 맞춰야함
+const REDIRECT_URI = 'http://shinjoeo.s3-website.ap-northeast-2.amazonaws.com/callback'; //백이랑 맞춰야함
 const AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 export {
